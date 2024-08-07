54e4ef62e63d633fadf88fe7c723b7f1ff23b782
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-08-07 15:06:38 +0100

    The second modified

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-#!/usr/bin/node
+/**
+ * This is the server file
+ */
+import express from 'express';
+import controllerRouting from './routes/index';
 
-const express = require("express");
-const router = require("./routes/index");
+const app = express();
+const port = process.env.PORT || 5000;
 
-const server = express();
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+app.use(express.json());
 
-server.use(express.json());
-server.use(router);
+controllerRouting(app);
 
-server.listen(PORT, () =>
-  console.log(`The server is running on port: ${PORT}`)
-);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
