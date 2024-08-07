27c90ba2496130e9fc8b4561d4b2fd6d4b0bec85
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-08-07 13:14:29 +0100

    The needed files

---
+++ controllers/UsersController.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/**
+ * here now is the user controller
+ */
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+      res.end();
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      res.end();
+      return;
+    }
+    const userExist = await dbClient.userExist(email);
+    if (userExist) {
+      res.status(400).json({ error: 'Already exist' });
+      res.end();
+      return;
+    }
+    const user = await dbClient.createUser(email, password);
+    const id = `${user.insertedId}`;
+    res.status(201).json({ id, email });
+    res.end();
+  }
+}
+
+module.exports = UsersController;
